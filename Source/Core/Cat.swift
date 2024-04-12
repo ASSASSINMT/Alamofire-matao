af96539ac38541a233a11cb90de18fd1dfa1797b
matao <6317488@any3.com>
2024-04-12 10:37:05 +0800

    添加cat类

---
+++ Source/Core/Cat.swift
@@ -0,0 +1,19 @@
+//
+//  Cat.swift
+//  Alamofire
+//
+//  Created by apple on 2024/4/12.
+//
+
+import Foundation
+
+public class Cat {
+    
+    public let name: String
+    public let age: Int
+    
+    public init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
